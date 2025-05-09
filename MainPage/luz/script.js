978e26bd104bdab9962fd347abe87fe66a89caae
Vitor Harthmann <151964698+devVHarthmann@users.noreply.github.com>
2025-05-09 17:56:03 -0300

    Add files via upload

---
+++ MainPage/luz/script.js
@@ -0,0 +1,7 @@
+function turn(){
+    if(document.getElementById('on').style.visibility == "hidden"){
+    document.getElementById('on').style.visibility = "visible"
+    } else{
+        document.getElementById('on').style.visibility = "hidden"
+    }
+}
---
+++ MainPage/script.js
@@ -0,0 +1,9 @@
+function redirect1(){
+    location.href = "../pomodoro/pomodoro.html"
+}
+function redirect2(){
+    location.href = "../luz/luz.html"
+}
+function redirect3(){
+    location.href = "../economias/economia.html"
+}
