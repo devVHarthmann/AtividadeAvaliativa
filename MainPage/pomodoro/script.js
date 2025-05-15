b18fcaa3715acc9b1b79728b0fb90397617b24a5
devVHarthmann <vdasilvaharthamnn@gmail.com>
2025-05-15 17:02:34 -0300

    crono

--- MainPage/pomodoro/script.js
+++ MainPage/pomodoro/script.js
@@ -11,14 +11,44 @@ btnDl.addEventListener('click', () => {
     setTimeout(() => {
         tB.style.pointerEvents = 'all';
     }, 440);
-    if(bgEstiloAtual == 'rgb(20, 20, 20)'){
+    if (bgEstiloAtual == 'rgb(20, 20, 20)') {
         land.style.backgroundColor = 'white'
-        hd.style.backgroundColor = 'rgb(199, 199, 199)'
+        hd.style.backgroundColor = 'rgb(219, 217, 216)'
         t1.style.color = 'black'
-    } else{
+        document.body.style.backgroundColor = 'white'
+    } else {
         land.style.backgroundColor = 'rgb(20, 20, 20)'
         hd.style.backgroundColor = ''
         t1.style.color = 'white'
+        document.body.style.backgroundColor = 'rgb(20, 20, 20)'
 
     }
 })
+
+document.getElementById('start').addEventListener('click', () => {
+    let duracao = 25 * 60;
+    display = document.getElementById('ctRlg')
+    document.getElementById('start').style.pointerEvents = 'none'
+    cronometro(duracao, display)
+})
+
+const cronometro = (duracao, display) => {
+    let cronometro = duracao;
+    let minutos, segundos;
+
+    let intervalo = setInterval(() => {
+        minutos = Math.floor(cronometro / 60);
+        segundos = Math.floor(cronometro % 60);
+
+        minutos = minutos < 10 ? '0' + minutos : minutos;
+        segundos = segundos < 10 ? '0' + segundos : segundos;
+
+        display.innerHTML = `${minutos}:${segundos}`
+        cronometro -= 1;
+
+        if (cronometro < 0) {
+            clearInterval(intervalo)
+            document.getElementById('start').style.pointerEvents = 'all'
+        }
+    }, 1000);
+}
