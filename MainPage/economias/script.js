c4debb150afcf2c90326da4d6ed03f18339b4218
devVHarthmann <vdasilvaharthamnn@gmail.com>
2025-05-16 22:53:53 -0300

    DeuPorHoje

--- MainPage/economias/script.js
+++ MainPage/economias/script.js
@@ -1,6 +1,6 @@
 let valorTotal = 0;
 let restante = 50;
-let quantMoedas = [0, 0, 0, 0, 0]
+
 
 document.getElementById('zero').addEventListener('mouseover', () => {
     document.getElementById('zero').style.backgroundColor = "rgba(0, 102, 255, 0.404)"
@@ -8,30 +8,6 @@ document.getElementById('zero').addEventListener('mouseover', () => {
 document.getElementById('zero').addEventListener('mouseout', () => {
     document.getElementById('zero').style.backgroundColor = "#868686"
 })
-
-function contaMoedas(valor) {
-    
-    if (valor == 0.05) {
-        quantMoedas[0] += 1
-    } if (valor == 0.10) {
-        quantMoedas[1] += 1
-    } if (valor == 0.25) {
-        quantMoedas[2] += 1
-    } if (valor == 0.50) {
-        quantMoedas[3] += 1
-    } if(valor == 1.00){
-        quantMoedas[4] +=1
-    }
-    attMoedas()
-}
-function attMoedas(){
-    document.getElementById('m1').innerHTML = quantMoedas[0]
-    document.getElementById('m2').innerHTML = quantMoedas[1]
-    document.getElementById('m3').innerHTML = quantMoedas[2]
-    document.getElementById('m4').innerHTML = quantMoedas[3]
-    document.getElementById('m5').innerHTML = quantMoedas[4]
-}
-
 
 function altImg() {
     if (valorTotal >= 0 && valorTotal < 12.5) {
@@ -63,14 +39,12 @@ function addValue(valor) {
         document.getElementById('show').innerHTML = `R$ ${valorTotal.toFixed(2)}`
         alert("Bolsa cheia")
         restante -= valor;
-        contaMoedas(valor)
-        attMoedas()
+
     } else {
         valorTotal += valor;
         document.getElementById('show').innerHTML = `R$ ${valorTotal.toFixed(2)}`
         restante -= valor;
-        contaMoedas(valor)
-        attMoedas()
+
     }
 
     if (restante < 1) {
@@ -105,12 +79,7 @@ function zerar() {
     document.getElementById('add3').removeAttribute('style')
     document.getElementById('add4').removeAttribute('style')
     document.getElementById('add5').removeAttribute('style')
-    quantMoedas[0] = 0
-    quantMoedas[1] = 0
-    quantMoedas[2] = 0
-    quantMoedas[3] = 0
-    quantMoedas[4] = 0
-    attMoedas()
+
 }
 
 function sacar() {
